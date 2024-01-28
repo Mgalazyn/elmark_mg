bf3ca79cf264ddb8ff6f28557658932cc0055b46
Mgalazyn <m.galazynn@gmail.com>
2024-01-28 20:54:27 +0100

    much straight forward solution as option 2

--- api/urls.py
+++ api/urls.py
@@ -10,8 +10,21 @@ urlpatterns = [
     path('location/<int:pk>/', views.LocationRUD.as_view()),
     path('parts/', views.PartsRW.as_view()),
     path('parts/<str:pk>/', views.PartRUD.as_view()),
-    path('parts-filter/', views.PartsFilter.as_view()), #alternative endpoint for filters
-    #DOCS
+
+    # path('parts-filter/', views.PartsFilter.as_view()), #alternative endpoint for filters
+    
+    # #DOCS
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
+    # #Other way if we want have separated endpoints, much straight forward
+    # path('parts/', views.PartsG.as_view()),
+    # path('parts/create/', views.PartsCR.as_view()),
+    # path('parts/update/<str:pk>/', views.PartsUD.as_view()),
+    # path('location', views.LocationG.as_view()),
+    # path('location/create/', views.LocationCR.as_view()),
+    # path('location/update/<int:pk>/', views.LocationUD.as_view()),
+    # path('category/', views.CategoryG.as_view()),
+    # path('category/create/', views.CategoryCR.as_view()),
+    # path('category/update/<int:pk>/', views.CategoryUD.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -6,7 +6,7 @@ from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
-
+#1 option, much more scalable and complex
 class PartsRW(generics.ListCreateAPIView):
     serializer_class = PartsSerializer
     queryset = Parts.objects.all()
@@ -40,8 +40,55 @@ class CategoryRUD(generics.RetrieveUpdateDestroyAPIView):
 
 
 #alternative endpoint for filters
-class PartsFilter(generics.ListAPIView):
-    queryset = Parts.objects.all()
-    serializer_class = PartsSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name', 'category', 'price', 'location']
+# class PartsFilter(generics.ListAPIView):
+#     queryset = Parts.objects.all()
+#     serializer_class = PartsSerializer
+#     filter_backends = [DjangoFilterBackend]
+#     filterset_fields = ['name', 'category', 'price', 'location']
+
+
+#2 much more straight forward option
+
+# class PartsG(generics.ListAPIView):
+#     serializer_class = PartsSerializer
+#     queryset = Parts.objects.all()
+
+
+# class PartsCR(generics.ListCreateAPIView):
+#     serializer_class = PartsSerializer
+#     queryset = Parts.objects.all()
+
+
+# class PartsUD(generics.RetrieveUpdateDestroyAPIView):
+#     serializer_class = PartsSerializer
+#     queryset = Parts.objects.all()
+
+
+# class LocationG(generics.ListAPIView):
+#     serializer_class = LocationSerializer
+#     queryset = Location.objects.all()
+
+
+# class LocationCR(generics.ListCreateAPIView):
+#     serializer_class = LocationSerializer
+#     queryset = Location.objects.all()
+
+
+# class LocationUD(generics.RetrieveUpdateDestroyAPIView):
+#     serializer_class = LocationSerializer
+#     queryset = Location.objects.all()
+
+
+# class CategoryG(generics.ListAPIView):
+#     serializer_class = CategoriesSerializer
+#     queryset = Categories.objects.all()
+
+
+# class CategoryCR(generics.ListCreateAPIView):
+#     serializer_class = CategoriesSerializer
+#     queryset = Categories.objects.all()
+
+
+# class CategoryUD(generics.RetrieveUpdateDestroyAPIView):
+#     serializer_class = CategoriesSerializer
+#     queryset = Categories.objects.all()
